b7ec4460f9ad3338201e8dbeb7fd7aa4a43547fb
George13100 <georgeboles2006@gmail.com>
2025-05-24 21:00:40 +0300

    Delete proj directory

--- proj/js/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-
-// supabaseClient.js
-const SUPABASE_URL =
- 'https://umgwaqoazwbczepffhxg.supabase.co';
-const SUPABASE_ANON_KEY = 
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtZ3dhcW9hendiY3plcGZmaHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMjU0ODYsImV4cCI6MjA2MzYwMTQ4Nn0.Gjt0GrOhd0e2NXiVSRg82xoyp4ijJpuHAxVNhqlHL-0';
-
-const supabase =
- supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
